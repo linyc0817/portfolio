70925ba02a8373aea464c8709b55ef5173b8c436
linyc <barney0817.cs10@nycu.edu.tw>
2025-03-14 14:03:44 +0800

    Update gitignore and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withMDX(nextConfig);
